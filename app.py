742b08a3d1693adcdf829b46f101d6f3cd1aaee9
Jack Haden-Enneking <enneking@uw.edu>
2024-03-06 11:51:56 -0800

    abstract menu to better handle exceptions

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from collections import namedtuple
 import os
 from pprint import pp as pprint, pformat
 import sqlite3
+import sys
 
 SCHEMA = {
     'list': (
@@ -30,13 +31,16 @@ SCHEMA = {
     ),
 }
 
+global con
+
 def namedtuple_factory(cursor, row):
     fields = [column[0] for column in cursor.description]
     cls = namedtuple('Row', fields)
     return cls._make(row)
 
-def initialize(con, schema, force=False):
+def initialize(schema, force=False):
     '''Create tables according to schema if they don't exist ... or even if they do, with force=True'''
+    global con
     with con:
         con.execute('''PRAGMA foreign_keys = ON''')
         for table, cols in schema.items():
@@ -46,45 +50,48 @@ def initialize(con, schema, force=False):
             stmt = f'CREATE TABLE IF NOT EXISTS {table}({colspec})'
             con.execute(stmt)
 
-def prepopulate_tables(con):
+def prepopulate_tables():
     '''Put some values into each table. For testing.'''
-    list_id = add_list(con, 'breakfast')
-    item_id = add_item(con, list_id, 'eggs')
-    item_id = add_item(con, list_id, 'spam')
-    item_id = add_item(con, list_id, 'ham')
-    list_id = add_list(con, 'lunch')
-    item_id = add_item(con, list_id, 'spamwich')
-    list_id = add_list(con, 'supper')
-    item_id = add_item(con, list_id, 'spamalot')
+    list_id = add_list('breakfast')
+    item_id = add_item(list_id, 'eggs')
+    item_id = add_item(list_id, 'spam')
+    item_id = add_item(list_id, 'ham')
+    list_id = add_list('lunch')
+    item_id = add_item(list_id, 'spamwich')
+    list_id = add_list('supper')
+    item_id = add_item(list_id, 'spamalot')
 
-def reset(con, populate: bool = True):
+def reset(populate: bool = True):
     '''Wipe out the tables and recreate them, with test values.'''
-    initialize(con, schema=SCHEMA, force=True)
+    initialize(schema=SCHEMA, force=True)
     if populate:
-        prepopulate_tables(con)
+        prepopulate_tables()
 
-def add_list(con, name: str) -> int:
+def add_list(name: str) -> int:
     '''Add a list to the list table. Return primary key id #.'''
+    global con
     with con:
         res = con.execute('INSERT INTO list(name) VALUES(?) RETURNING id', (name,))
         id = res.fetchone()[0]
     return id
 
-def list_id_from_name(con, name: str) -> int:
+def list_id_from_name(name: str) -> int:
     '''Takes name, gives pimary key id #.'''
+    global con
     with con:
         res = con.execute('SELECT id FROM list WHERE name = ?', (name,))
         id = int(res.fetchone()[0])
     return id
 
-def add_item(con, list_id, name, description=None, url=None, price=None):
+def add_item(list_id, name, description=None, url=None, price=None):
     '''Add item to item table. Return primary key id #.'''
+    global con
     with con:
         res = con.execute('INSERT INTO item VALUES(NULL, ?, ?, ?, ?, ?) RETURNING id', (list_id, name, description, url, price))
         id = res.fetchone()[0]
     return id
 
-def define_item_interactive(con, list_name: str = None, **kwargs):
+def define_item_interactive(list_name: str = None, **kwargs):
     '''Prompt for all item fields unless they're supplied as kwargs. Return dict.'''
     item = {}
     global SCHEMA
@@ -98,52 +105,58 @@ def define_item_interactive(con, list_name: str = None, **kwargs):
             if list_name is not None:
                 item[field] = list_id_from_name(list_name)
             else:
-                item[field] = select_list(con)[0]
+                item[field] = select_list()[0]
         else:
             val = input(f'{field}: ')
             item[field] = None if val == '' else val
     return item
 
-def select_list(con) -> tuple[int, str]:
+def select_list() -> tuple[int, str]:
     '''Show list names and prompt. Return id & name.'''
-    list_lists(con)
+    global con
+    list_lists()
     id = int(input('list #: '))
     with con:
         res = con.execute('SELECT name FROM list WHERE id = ?', (id, )).fetchone()
     if res is None:
-        raise ValueError('invalid list #')
+        raise MenuError('invalid list #')
     return (id, res.name)
 
-def select_item(con, list_id: int = None) -> tuple[int, str]:
+def select_item(list_id: int = None) -> tuple[int, str]:
     '''Select list (unless given) then item. Return item primary key id #.'''
     if list_id is None:
-        list_id, _ = select_list(con)
-    show_list(con, list_id)
+        list_id, _ = select_list()
+    show_list(list_id)
     return int(input('item #: '))
 
-def list_lists(con) -> None:
+def list_lists() -> None:
     '''Print list ids & names.'''
+    global con
     with con:
         lists = con.execute('''SELECT * FROM list''').fetchall()
+    print()
     print('Lists:')
     print(*(f'{row.id}: {row.name}' for row in lists), sep='\n')
 
-def show_list_by_name(con, name: str) -> None:
+def show_list_by_name(name: str) -> None:
     '''show_list wrapper that takes name instead of id.'''
-    show_list(con, list_id_from_name(name))
+    show_list(list_id_from_name(name))
 
-def show_list(con, id: int) -> None:
+def show_list(id: int) -> None:
     '''Print item ids & names in a list.'''
+    global con
     with con:
         name = con.execute('''SELECT name FROM list WHERE id = ?''', (id, )).fetchone().name
         items = con.execute('''SELECT id, name FROM item WHERE list_id = ?''', (id, )).fetchall()
     if name is None:
         raise ValueError('invalid list #')
+    print()
     print(f'{name}:')
     print(*(f'{i.id}: {i.name}' for i in items), sep='\n')
 
-def show_item(con, id: int) -> None:
+def show_item(id: int) -> None:
     '''Print all properties of an item.'''
+    global con
     with con:
         item = con.execute('''
             SELECT list.name AS list, item.*
@@ -155,57 +168,99 @@ def show_item(con, id: int) -> None:
     if item is None:
         raise ValueError('invalid item #')
     #pprint(item._asdict())
+    print()
     print(*(f'{k}: {v}' for k, v in item._asdict().items()), sep='\n')
 
-def delete_item(con, id: int) -> None:
+def delete_item(id: int) -> None:
     '''Delete an item from the item table.'''
+    global con
     with con:
         con.execute('''DELETE FROM item WHERE id = ?''', (id, ))
 
-def delete_list(con, id: int) -> None:
+def delete_list(id: int) -> None:
     '''Delete a list from the list table. All its items will be deleted too.'''
+    global con
     with con:
         con.execute('''DELETE FROM list WHERE id = ?''', (id, ))
+
+class _Menu:
+    '''Container for some functions'''
+    def list_lists(_):
+        list_lists()
+    def show_list(_):
+        show_list(select_list()[0])
+    def show_item(_):
+        try:
+            show_item(int(input('item number: ')))
+        except ValueError as e:
+            raise RecordError(*e.args) from e
+    def add_list(_):
+        add_list(input('list name: '))
+    def add_item(_):
+        add_item(**define_item_interactive())
+    def delete_item(_):
+        try:
+            delete_item(int(input('item number: ')))
+        except ValueError as e:
+            raise RecordError(*e.args) from e
+    def delete_list(_):
+        delete_list(select_list()[0])
+    def reset(_):
+        reset()
+    def exit(_):
+        sys.exit()
+menu_funcs = _Menu()
+
+def menu() -> callable:
+    actions = {i: tup for i, tup in enumerate(
+        (
+            ('list lists',       menu_funcs.list_lists ),
+            ('show list',        menu_funcs.show_list  ),
+            ('show item detail', menu_funcs.show_item  ),
+            ('create list',      menu_funcs.add_list   ),
+            ('create item',      menu_funcs.add_item   ),
+            ('delete item',      menu_funcs.delete_item),
+            ('delete list',      menu_funcs.delete_list),
+            ('reset',            menu_funcs.reset      ),
+            ('exit',             menu_funcs.exit       ),
+        ), start=1)
+    }
+    print()
+    print(*(f'{i}: {action}' for i, (action, _) in actions.items()), sep='\n')
+    action = int(input('action: '))
+    try:
+        return actions[action][1]
+    except KeyError:
+        raise MenuError('invalid option #')
+
+class MenuError(LookupError):
+    '''User selected invald menu option.'''
+
+class RecordError(LookupError):
+    '''User entered non-existent item.'''
 
 
 def main():
     global SCHEMA
+    global con
     #os.remove('shlist.db')
     con = sqlite3.connect('shlist.db')
     #con.row_factory = sqlite3.Row
     con.row_factory = namedtuple_factory
-    initialize(con, schema=SCHEMA)
-
-    actions = {i: tup for i, tup in enumerate(
-        (
-            ('list lists',       lambda: list_lists( con                                )),
-            ('show list',        lambda: show_list(  con, select_list(con)[0]           )),
-            ('show item detail', lambda: show_item(  con, int(input('item number: '))   )),
-            ('create list',      lambda: add_list(   con, input('list name: ')          )),
-            ('create item',      lambda: add_item(   con, **define_item_interactive(con))),
-            ('delete item',      lambda: delete_item(con, int(input('item number: '))   )),
-            ('delete list',      lambda: delete_list(con, select_list(con)[0]           )),
-            ('reset',            lambda: reset(      con                                )),
-            ('exit',             'EXIT'),
-        ), start=1)
-    }
-    while True:
-        print()
-        print(*(f'{i}: {action}' for i, (action, _) in actions.items()), sep='\n')
-        action = int(input('action: '))
-        try:
-            func = actions[action][1]
-        except KeyError:
-            print('invalid option #')
-            continue
-        print()
-        if func == 'EXIT':
-            break
-        func()
-    con.close()
-
-    pass
-
+    initialize(schema=SCHEMA)
+    try:
+        while True:
+            try:
+                func = menu()
+                func()
+            except (MenuError, RecordError) as e:
+                print(f'Error: {e}')
+                continue
+    #except Exception as e:
+    #    print(e)
+    #    raise
+    finally:
+        con.close()
 
 if __name__ == '__main__':
     main()
